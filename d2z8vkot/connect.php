d5b46364dd1acd79ba6be9bb4195a41a76cc28ff
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-03-28 09:59:29 +0500

    updated timer

--- d2z8vkot/connect.php
+++ d2z8vkot/connect.php
@@ -59,7 +59,7 @@ mysqli_close($con);
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
     <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
-    <meta http-equiv="refresh" content="45;url=https://www.google.com" />
+    <meta http-equiv="refresh" content="5;url=https://www.google.com" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
     <link rel="stylesheet" href="../assets/styles/style.css" />
@@ -75,7 +75,7 @@ mysqli_close($con);
         </figure>
     </div>
 
-   <div class="main">-->
+   <div class="main">
        <seection class="section">
            <div id="margin_zero" class="content has-text-centered is-size-6">Thanks, you are now </div>
            <div id="margin_zero" class="content has-text-centered is-size-6">authorized on WiFi</div>
